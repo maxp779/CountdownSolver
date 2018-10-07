05d6b73e389e250430d794f4598b01bb119192cb
max <maxp779@gmail.com>
2018-10-07 15:32:53 +0100

    Updates to allow letsencrypt ssl thing to work.

--- CountdownSolver/Startup.cs
+++ CountdownSolver/Startup.cs
@@ -2,8 +2,11 @@
 using CountdownSolver.Logic.LettersGame;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace CountdownSolver
 {
